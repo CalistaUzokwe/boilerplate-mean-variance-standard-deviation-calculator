d77bdd028183b38d5e6b7fafbd24c3df1e22c5bd
Calista Ngozi  Uzokwe <calistauzokwe@yahoo.com>
2025-03-27 15:09:37 +0000

    Saving file

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def calculate(lst):
     if len(lst) != 9:
-        raise ValueError("list must contain nine numbers.", mean_var_std.calculate, [2,6,2,8,4,0,1,])
+        raise ValueError("list must contain nine numbers")
 
     matrix = np.array(lst).reshape(3, 3)
 
